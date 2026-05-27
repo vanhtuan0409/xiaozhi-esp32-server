17b94c356576668171e2d247a1b0ee1d4c84fc5b
Tuan Vuong <vanhtuan0409@gmail.com>
2026-05-27 16:57:08 +0700

    adding vieneu tts

--- run-dev.sh
+++ run-dev.sh
@@ -10,7 +10,8 @@ docker run --rm -it \
   --name "${CONTAINER_NAME}" \
   --security-opt seccomp=unconfined \
   -e TZ=Asia/Ho_Chi_Minh \
-  -e LLAMA_CPP_TOKEN=${LLAMA_CPP_TOKEN}\
+  -e LLAMA_CPP_TOKEN=${LLAMA_CPP_TOKEN} \
+  -e HF_HOME=/opt/xiaozhi-esp32-server/models/hf \
   -p 8000:8000 \
   -p 8003:8003 \
   -v "${SERVER_DIR}:/opt/xiaozhi-esp32-server" \
