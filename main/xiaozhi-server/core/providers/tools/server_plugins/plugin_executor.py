ecdddb52b5c81232c8b10045758734c66e306e3c
Tuan Vuong <vanhtuan0409@gmail.com>
2026-06-03 22:33:53 +0700

    disable get_lunar tools

--- main/xiaozhi-server/core/providers/tools/server_plugins/plugin_executor.py
+++ main/xiaozhi-server/core/providers/tools/server_plugins/plugin_executor.py
@@ -54,7 +54,7 @@ class ServerPluginExecutor(ToolExecutor):
         tools = {}
 
         # 获取必要的函数
-        necessary_functions = ["handle_exit_intent", "get_lunar"]
+        necessary_functions = ["handle_exit_intent"]
 
         # 获取配置中的函数
         config_functions = self.config["Intent"][
